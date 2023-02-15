16325741c5a7076183d6ac67f18fc6faddeac308
fanden-git <fanden@mail.ru>
2023-02-15 21:29:07 +0300

    add urls.py in "app"

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from app.views import home_view, time_view, workdir_view
+from django.urls import path, include
 
 
 urlpatterns = [
-    path('', home_view, name='home'),
-    path('current_time/', time_view, name='time'),
-    path('workdir/', workdir_view, name='workdir'),
     path('admin/', admin.site.urls),
+    path('', include('app.urls')),
 ]
